e1be7980ce9aa5f1fc7923b7ddeaffeb2f31d5a2
Jonatan Sombié <jonatan.sombie@gmail.com>
2023-06-02 15:02:01 +0200

    use flexible version constraints

--- auth0-oidc/examples/simple/main.tf
+++ auth0-oidc/examples/simple/main.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     random = {
       source  = "registry.terraform.io/hashicorp/random"
-      version = "3.2.0"
+      version = "~>3.0"
     }
 
     auth0 = {
       source  = "registry.terraform.io/auth0/auth0"
-      version = "0.32.0"
+      version = "~>0.32"
     }
   }
 }
