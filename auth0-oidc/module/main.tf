8dc0e0b10b96b9ec93adfdd8ba81a6d46e8f6300
Jonatan Sombié <jonatan.sombie@gmail.com>
2023-06-02 15:43:00 +0200

    fix random name creation

--- auth0-oidc/module/main.tf
+++ auth0-oidc/module/main.tf
@@ -15,12 +15,8 @@ terraform {
 data "aws_region" "current" {}
 data "auth0_tenant" "current" {}
 
-locals {
-  app_name_prefix = var.app_name_prefix != null ? var.app_name_prefix : ""
-}
-
 resource "random_pet" "client_name" {
-  prefix = local.app_name_prefix
+  prefix = var.app_name_prefix
 }
 
 resource "auth0_client" "client" {
